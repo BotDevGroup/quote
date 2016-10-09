4a74efe3f69ff99c1bab6cf95cb0a38d06df3cd5
Jorge Dominguez <jorge@innova-code.com>
2016-10-09 18:45:41 -0400

    chaging name to config.py its can load any other configuration the plugin need

---
+++ quote/default_config.py
@@ -0,0 +1,6 @@
+
+#insert your API key
+#you can get your apikey from https://market.mashape.com/andruxnet/random-famous-quotes
+#rename this file from default_config.py to config.py so it can work.
+
+api_key = ""
