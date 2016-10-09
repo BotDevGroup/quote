fa94dde468d26d7ed308605c1e5572e4a16c519e
Jorge Dominguez <jorge@innova-code.com>
2016-10-09 18:27:06 -0400

    two new commands /moviequote and /famousequote and cleaning the api key from the file

--- quote/tasks.py
+++ quote/tasks.py
@@ -4,14 +4,15 @@ from marvinbot.handlers import Filters, CommandHandler, MessageHandler
 from celery.utils.log import get_task_logger
 from celery import task
 import requests
+from api_key import api_key
 
 log = get_task_logger(__name__)
 adapter = get_adapter()
-
+print(api_key)
 
 def quote_movie(update, *args):
     response = requests.get('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies', headers={
-      "X-Mashape-Key": "BEjqsVLNGDmshmQ0ZxP0TC6j7wfDp1tlihvjsnUhhELDOJA7f5",
+      "X-Mashape-Key": api_key,
       "Content-Type": "application/x-www-form-urlencoded",
       "Accept": "application/json"
     })
@@ -21,9 +22,22 @@ def quote_movie(update, *args):
                                                                   author=r['author']),
                             parse_mode='Markdown')
 
+def quote_famous(update, *args):
+    response = requests.get('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous', headers={
+      "X-Mashape-Key": api_key,
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    })
+    r = response.json()
+    adapter.bot.sendMessage(chat_id=update.message.chat_id,
+                            text="_{quote}_ -- *{author}*".format(quote=r['quote'],
+                                                                  author=r['author']),
+                            parse_mode='Markdown')
 
 def setup(new_adapter):
     global adapter
     adapter = new_adapter
 
-    adapter.add_handler(CommandHandler('quote', quote_movie, call_async=True))
+    adapter.add_handler(CommandHandler('moviequote', quote_movie, call_async=True))
+    adapter.add_handler(CommandHandler('famousquote', quote_famous, call_async=True))
+
